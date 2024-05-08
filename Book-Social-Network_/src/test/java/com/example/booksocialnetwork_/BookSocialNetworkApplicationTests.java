76cd3f1aaea076a218a9976e0494ff16280f6730
HananSaeed <1900948@eng.asu.edu.eg>
2024-05-08 20:36:32 +0200

    intial-commit

---
+++ Book-Social-Network_/src/test/java/com/example/booksocialnetwork_/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksocialnetwork_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
